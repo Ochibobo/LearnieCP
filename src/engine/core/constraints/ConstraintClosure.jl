50e84dedbfd7e67036ac91d439e231471d0893f4
Warren Ochibobo <ochibobowarren@gmail.com>
2023-07-07 14:41:36 +0300

    Added the constraint closure struct

---
+++ src/engine/core/constraints/ConstraintClosure.jl
@@ -0,0 +1,56 @@
+using Parameters
+
+"""
+    struct ConstraintClosure <: AbstractConstraint
+        sm::StateManager
+        filteringFunction::Function
+        scheduled::Bool
+        state::State
+
+        function ConstraintClosure(sm::StateManager, fn)
+            state = makeStateRef(sm, true)
+
+            new(sm, fn, false, state)
+        end
+    end
+
+`ConstraintClosure` used to store anonymous functions to be executed upon propagation
+"""
+@with_kw struct ConstraintClosure <: AbstractConstraint
+    sm::StateManager
+    filteringFunction::Function
+    scheduled::Bool
+    state::State
+
+    function ConstraintClosure(sm::StateManager, fn)
+        state = makeStateRef(sm, true)
+
+        new(sm, fn, false, state)
+    end
+end
+
+
+"""
+    post(c::ConstraintClosure)::Nothing
+
+Function to post a `ConstraintClosure`
+"""
+function post(c::ConstraintClosure)::Nothing
+    _ = c
+
+    return nothing
+end
+
+
+"""
+    propagate(c::ConstraintClosure)::Nothing
+
+Function to propagate a `ConstraintClosure`
+"""
+function propagate(c::ConstraintClosure)::Nothing
+    ## Execute the filtering function
+    c.filteringFunction()
+
+    return nothing
+end
+
