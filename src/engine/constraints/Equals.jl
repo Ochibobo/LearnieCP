caaa2fe057783d35252ad010c3e6391e2bdab364
Warren Ochibobo <ochibobowarren@gmail.com>
2023-07-28 20:52:04 +0300

    Fixed variable errors, max function to min and in function variable ordering

--- src/engine/constraints/Equals.jl
+++ src/engine/constraints/Equals.jl
@@ -32,20 +32,20 @@ function post(c::Equal)::Nothing
         Variables.fix(c.x, Variables.minimum(c.y))
     else
         ## Ensure the bounds intersect
-        boundsIntersect(x, y)
+        boundsIntersect(c.x, c.y)
         ## Prune the domains of the variables; removing values that are different in either domain
-        pruneEquals(x, y)
-        pruneEquals(y, x)
+        pruneEquals(c.x, c.y)
+        pruneEquals(c.y, c.x)
         
         ## Register the constraints to be propagated as anonymous constraints
-        whenDomainChange(x, () -> begin
-            boundsIntersect(x, y) ## Ascertain the bounds intersect
-            pruneEquals(x, y)     ## Prune values within the domains that aren't equal
+        whenDomainChange(c.x, () -> begin
+            boundsIntersect(c.x, c.y) ## Ascertain the bounds intersect
+            pruneEquals(c.x, c.y)     ## Prune values within the domains that aren't equal
         end)
 
-        whenDomainChange(y, () -> begin
-            boundsIntersect(y, x) ## Ascertain the bounds intersect
-            pruneEquals(y, x)     ## Prune values within the domains that aren't equal
+        whenDomainChange(c.y, () -> begin
+            boundsIntersect(c.y, c.x) ## Ascertain the bounds intersect
+            pruneEquals(c.y, c.x)     ## Prune values within the domains that aren't equal
         end)
     end
 
@@ -61,7 +61,7 @@ Ascertain that the bounds of variables `x` and `y` are the same/intersect
 function boundsIntersect(x::AbstractVariable{T}, y::AbstractVariable{T})::Nothing where T
     ## Get the maximum and minimim of both variables
     maxMinumimValue = max(Variables.minimum(x), Variables.minimum(y)) ## The greatest minimum
-    minMaximumValue = max(Variables.maximum(x), Variables.maximum(y)) ## The smallest maximum
+    minMaximumValue = min(Variables.maximum(x), Variables.maximum(y)) ## The smallest maximum
 
     ## Align the bounds
     Variables.removeAbove(x, minMaximumValue)
@@ -80,14 +80,14 @@ Function to prune values that aren't the same in the domain of the variables `x`
 """
 function pruneEquals(from::AbstractVariable{T}, to::AbstractVariable{T})::Nothing where T
     ## Vector to hold domain values
-    target = Vector{T}(undef, max(Variables.size(from), Variables.size(to)))
+    target = Vector{T}()
 
     ## Fill the target vector
     Variables.fillArray(from, target)
 
     ## Actual pruning of values that aren't equal
     for value in target
-        if !Variables.in(from, value)
+        if !Variables.in(value, from)
             Variables.remove(to, value)
         end
     end
