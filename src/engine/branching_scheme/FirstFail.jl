d8c247be11a68a11419eea1345687af95a13e7e8
Warren Ochibobo <ochibobowarren@gmail.com>
2023-08-31 00:04:14 +0300

    Added selectMin and firstFail branching schemes

---
+++ src/engine/branching_scheme/FirstFail.jl
@@ -0,0 +1,44 @@
+"""
+    selectMin(v::Vector{AbstractVariable{T}}, fnTest::Function, fnComparison::Function)::Union{Nothing, AbstractVariable{T}} where T
+
+Function to select the minimum variable in `v` that satisfies `fnTest` or `nothing` otherwise
+"""
+function selectMin(v::Vector{AbstractVariable{T}}, fnTest::Function, fnComparison::Function)::Union{Nothing, AbstractVariable{T}} where T
+    selectedVar = nothing
+
+    for var in v
+        if fnTest(var)
+            selectedVar = isnothing(selectedVar) || (fnComparison(var, selectedVar)) ? var : selectedVar
+        end
+    end
+
+    return selectedVar
+end
+
+
+
+"""
+    firstFail(vars::Vararg{AbstractVariable{T}}) where T
+
+Function used to branch based on the first failed node as selected by the `selectMin` function
+"""
+function firstFail(vars::Vararg{AbstractVariable{T}})::Vector where T
+    return () -> begin
+        xVar = selectMin(collect(vars),
+            (var) -> !Variables.isFixed(var),
+            (varA, varB) -> size(varA) < size(varB)
+        )
+
+        ## If all variables are fixed, return an empty branching scheme
+        if isnothing(xVar)
+            return []
+        end
+        ## Get the minimum of the selected variable
+        xVarMin = minimum(xVar)
+
+        return [
+            () -> Solver.post(Variables.solver(xVar), ConstEqual{T}(xVar, xVarMin)),
+            () -> Solver.post(Variables.solver(xVar), ConstNotEqual{T}(xVar, xVarMin))
+        ]
+    end
+end
