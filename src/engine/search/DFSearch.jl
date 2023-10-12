d2abe6852d2e68233ab153663f2debbbbdc75d21
Warren Ochibobo <ochibobowarren@gmail.com>
2023-10-12 19:29:57 +0300

    Added methods to optimize subject to conditions, limits to search and SearchStatistics object

--- src/engine/search/DFSearch.jl
+++ src/engine/search/DFSearch.jl
@@ -1,13 +1,25 @@
 """
-DFS Search of the constraint solver
+    mutable struct DFSearch
+        sm::StateManager
+        branchingSchema::Function
+        onSolutionListeners::Vector{Function}
+        searchStatistics::SearchStatistics
+
+        function DFSearch(sm::StateManager, branchingSchema)
+            new(sm, branchingSchema, Vector{Function}(), SearchStatistics())
+        end
+    end
+
+`DFSearch` sturct for the constraint solver
 """
 @with_kw mutable struct DFSearch
     sm::StateManager
     branchingSchema::Function
     onSolutionListeners::Vector{Function}
+    searchStatistics::SearchStatistics
 
     function DFSearch(sm::StateManager, branchingSchema)
-        new(sm, branchingSchema, Vector{Function}())
+        new(sm, branchingSchema, Vector{Function}(), SearchStatistics())
     end
 end
 
@@ -52,11 +64,14 @@ end
 
 Function to solve the CSP
 """
-function solve(s::DFSearch)::Nothing
+function solve(s::DFSearch; searchLimit::Function = () -> false)::Nothing
     # withNewState(stateManager(s), () -> begin this was a replication of storage, don't know how impactful it was
-        dfs(s)
+    dfs(s, searchLimit = searchLimit)
+    ## Mark the search as being completed
+    markAsCompleted(s.searchStatistics)
     # end)
 end
+
 
 
 """
@@ -64,21 +79,32 @@ end
 
 Actual search function
 """
-function dfs(s::DFSearch)::Nothing
+function dfs(s::DFSearch; searchLimit::Function = () -> false)::Nothing
+    ## Execute the search-limit if met
+    if searchLimit()
+        throw(DomainError("Search limit met. Searching stopped."))
+    end
+
+    ## Get the branches from the branching schema
     branches = s.branchingSchema()
 
     if length(branches) == 0
         notifySolution(s)
+        increaseNumberOfSolutions(s.searchStatistics)
     else
         for branch in branches
             withNewState(stateManager(s), () -> begin
                 try
+                    ## Increase the number of nodes
+                    increaseNumberOfNodes(s.searchStatistics)
                     ## Execute the branch to propagate the constraints, like calling the fixpoint over here
                     branch()
                     ## Recursively call the DFS
                     dfs(s)
                 catch e
-                    ##println("Failure in search node with error $e")
+                    ## Increase the number of failures
+                    increaseNumberOfFailures(s.searchStatistics)
+                    ## println("Failure in search node with error $e")
                     ##throw(e)
                 end
             end)
@@ -92,10 +118,36 @@ end
 
 Function to optimize the results
 """
-function optimize(objective::AbstractObjective, s::DFSearch)::Nothing
+function optimize(objective::AbstractObjective, s::DFSearch; searchLimit::Function = () -> false)::Nothing
     addOnSolution(s, () -> tighten(objective))
     ## Run the dfs/solve
-    dfs(s)
+    solve(s, searchLimit = searchLimit)
 
     return nothing
+end
+
+
+"""
+    optimizeSubjectTo(objective::AbstractObjective, s::DFSearch;
+        searchLimit::Function = () -> false, subjectTo::Function = () -> {})::Nothing
+
+Function `optimizeSubjectTo` to optimize subject to certain limits
+"""
+function optimizeSubjectTo(objective::AbstractObjective, s::DFSearch;
+    searchLimit::Function = () -> false, subjectTo::Function = () -> nothing)::Nothing
+    ## Initialize the SearchStatistics
+    s.searchStatistics = SearchStatistics()
+    try
+        ## Function to be called
+        subjectTo()
+        ## Call the optimize function
+        optimize(objective, s, searchLimit = searchLimit)
+    catch e
+        if contains(string(e), "NoSuchElement")
+            throw(e)
+        end
+        println("Error when running subjective optimization: $e")
+        ## throw(e)
+        ## Will classify exceptions later
+    end
 end
