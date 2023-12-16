32f04cf5b34ccf06713da4ecb6400fde40d39eb1
Warren Ochibobo <ochibobowarren@gmail.com>
2023-12-17 02:41:42 +0300

    Added examples for BoolVar

--- src/examples/csp/boolvar_test.jl
+++ src/examples/csp/boolvar_test.jl
@@ -7,26 +7,55 @@ using .JuliaCP
 solver =  Engine.LearnieCP()
 
 ## Boolean variable example
-b = Engine.Variables.BoolVar(solver)
+# b = Engine.Variables.BoolVar(solver)
 
-Engine.maximum(b)
-Engine.minimum(b)
+# Engine.maximum(b)
+# Engine.minimum(b)
 
-## Fix b to 1
-Engine.fix(b, true)
+# ## Fix b to 1
+# Engine.fix(b, true)
 
-## Mark b as not
-r = Engine.Variables.not(b)
+# ## Mark b as not
+# r = Engine.Variables.not(b)
 
-Engine.maximum(r)
-Engine.minimum(r)
+# Engine.maximum(r)
+# Engine.minimum(r)
 
-j = Engine.Variables.not(r)
+# j = Engine.Variables.not(r)
 
-Engine.minimum(j)
-Engine.maximum(j)
+# Engine.minimum(j)
+# Engine.maximum(j)
 
-k = !j
+# k = !j
 
-Engine.minimum(k)
-Engine.maximum(k)
+# Engine.minimum(k)
+# Engine.maximum(k)
+
+
+a = Engine.Variables.BoolVar(solver)
+# b = Engine.Variables.BoolVar(solver)
+b = !a
+
+# Engine.post(solver, Engine.NotEqual{Integer}(a, b))
+maximum(a)
+minimum(a)
+maximum(b)
+minimum(b)
+
+Engine.post(solver, Engine.ConstEqual{Integer}(a, true))
+
+Engine.isFixed(a)
+Engine.isFixed(b)
+
+maximum(a)
+minimum(a)
+maximum(b)
+minimum(b)
+
+a = Engine.Variables.BoolVar(solver)
+Engine.post(solver, Engine.ConstEqual{Integer}(a, true))
+
+maximum(a)
+minimum(a)
+maximum(b)
+minimum(b)
