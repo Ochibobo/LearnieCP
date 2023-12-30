6051cebe936c83bb5e224283fbf4dc073da86d86
Warren Ochibobo <ochibobowarren@gmail.com>
2023-12-31 02:30:32 +0300

    Added comments to the Or constraint

--- src/engine/constraints/Or.jl
+++ src/engine/constraints/Or.jl
@@ -1,6 +1,34 @@
 """
+    @with_kw mutable struct Or <: AbstractConstraint
+        solver::AbstractSolver
+        bVars::Vector{BoolVar}
+        
+        ## Watch variables
+        wL::State
+        wR::State
 
-`Or` logical constraint for a vector of `BoolVar`. 
+        ## Constraint-wide variables
+        active::State
+        scheduled::Bool
+
+        function Or(bVars::Vector{BoolVar})
+            isempty(bVars) && throw(DomainError("bVars cannot be an empty vector"))
+
+            solver = Variables.solver(bVars[1])
+            sm = stateManager(solver)
+
+            wL = makeStateRef(sm, 1)
+            wR = makeStateRef(sm, length(bVars))
+
+            active = makeStateRef(sm, true)
+
+            new(solver, bVars, wL, wR, active, false)
+        end
+    end
+
+`Or` logical constraint for a vector of `BoolVar`. This constraint ensures that at least 1 variable in the `BoolVar`
+vector evaluates to `true`. If this is not possible, an `InconsistencyException` is thrown, meaning all variables
+in the vector are `false`.
 """
 @with_kw mutable struct Or <: AbstractConstraint
     solver::AbstractSolver
@@ -84,6 +112,7 @@ function propagate(c::Or)::Nothing
         throw(DomainError("Inconsistency exception violating the Or constraint"))
     elseif value(c.wL) == value(c.wR)
         ## A case for only one unassigned variable
+        ## Set it to true to satisfy the Or (this marks this variable as 1)
         Variables.fix(c.bVars[value(c.wL)], true)
         ## Deactivate the constraint
         setValue!(c.active, false)
